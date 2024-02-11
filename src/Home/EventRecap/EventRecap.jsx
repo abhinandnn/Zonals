4637183d5700ac80f569dfb066933f00267071a4
Abhinandan <abhinandnn21@gmail.com>
2024-02-11 23:00:13 +0530

    fixed about us section

--- src/Home/EventRecap/EventRecap.jsx
+++ src/Home/EventRecap/EventRecap.jsx
@@ -15,9 +15,9 @@ const EventRecap = () => {
                     <h1>Event Recap</h1>
                 </div>
                 <div className="par1">
-                <p class="pa">AKGEC organised the <b>25th edition</b> of the prestigious annual sports event, the <b>Ajay Kumar Garg Memorial Table Tennis Tournament</b>, under the aegis of the <b>Ghaziabad Association of Table Tennis (GATT)</b>, affiliated with the <b>Uttar Pradesh Table Tennis Association</b> in the college during <b>03-04 February 2024</b>. The <b>National Level Tournament</b> is held every year for all professional institutes, colleges, and schools of the country.</p>
-    <p class="pa">The tournament was participated by <b>24 teams</b> and <b>222 participants</b> in the singles category from various parts of India like <b>Uttar Pradesh, Haryana, and Delhi</b>. Participation by <b>State and National level players</b> raised the level of the game to high standards during the tournament.</p>
-    <p class="pa">The tournament was a <b>huge success</b> as young players mesmerized everyone with their skillful game. The winners cherished their moments while others vowed to come back <b>stronger next year</b>.</p>
+                <p className="pa">AKGEC organised the <b>25th edition</b> of the prestigious annual sports event, the <b>Ajay Kumar Garg Memorial Table Tennis Tournament</b>, under the aegis of the <b>Ghaziabad Association of Table Tennis (GATT)</b>, affiliated with the <b>Uttar Pradesh Table Tennis Association</b> in the college during <b>03-04 February 2024</b>. The <b>National Level Tournament</b> is held every year for all professional institutes, colleges, and schools of the country.</p>
+    <p className="par">The tournament was participated by <b>24 teams</b> and <b>222 participants</b> in the singles category from various parts of India like <b>Uttar Pradesh, Haryana, and Delhi</b>. Participation by <b>State and National level players</b> raised the level of the game to high standards during the tournament.</p>
+    <p className="par">The tournament was a <b>huge success</b> as young players mesmerized everyone with their skillful game. The winners cherished their moments while others vowed to come back <b>stronger next year</b>.</p>
                 </div>
                 
             </div>
@@ -115,7 +115,7 @@ const EventRecap = () => {
 </Table>
 </div>
 <div className="par1">
-<p className='pa'>The closing ceremony was held on <b>04, February 2024</b>. <b>Mr. Deepak Malik</b>, a businessman and sports enthusiast, was the chief guest. The presence of Honourable members of the Board of Governors, <b>Mr. Sudhir Agarwal</b> and <b>Mr. Vinay Garg</b>, Director General <b>Dr. R.K. Agarwal</b>, added to the importance of the event. <b>Mr. Rajeev Sharma</b>, the organizing secretary, presented the Tournament brief. Heads of Departments, Deans, Section Incharges, Faculty, staff, and students of AKGEC along with members of Ghaziabad Table Tennis Association witnessed the prize distribution ceremony. Organizing Head <b>Prof I.P. Sharma</b> thanked all present for sparing their time.</p>
+<p className='par'>The closing ceremony was held on <b>04, February 2024</b>. <b>Mr. Deepak Malik</b>, a businessman and sports enthusiast, was the chief guest. The presence of Honourable members of the Board of Governors, <b>Mr. Sudhir Agarwal</b> and <b>Mr. Vinay Garg</b>, Director General <b>Dr. R.K. Agarwal</b>, added to the importance of the event. <b>Mr. Rajeev Sharma</b>, the organizing secretary, presented the Tournament brief. Heads of Departments, Deans, Section Incharges, Faculty, staff, and students of AKGEC along with members of Ghaziabad Table Tennis Association witnessed the prize distribution ceremony. Organizing Head <b>Prof I.P. Sharma</b> thanked all present for sparing their time.</p>
         </div>
         </div>
     )
